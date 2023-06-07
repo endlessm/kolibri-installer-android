0b6034076e79b9486109b2cabcc453310b2a082f
Dylan McCall <dylan@endlessos.org>
2023-06-07 09:49:27 -0700

    Configure PythonActivity to open external links in a browser
    A recent change to python-for-android requires that we set the
    activity's mOpenExternalLinksInBrowser attribute to true.
    
    Closes #133

--- src/java/org/learningequality/KolibriAndroidHelper.java
+++ src/java/org/learningequality/KolibriAndroidHelper.java
@@ -76,6 +76,8 @@ public class KolibriAndroidHelper {
     public void configure(final Runnable startWithNetwork, final Runnable startWithUSB, final Runnable loadingReady) {
         Log.i(TAG, "KolibriAndroidHelper configure");
 
+        mActivity.mOpenExternalLinksInBrowser = true;
+
         mLoadingWebView.setWebViewClient(new WebViewClient() {
             private boolean mInWelcome = false;
 
