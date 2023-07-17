e241322c2628500297f47202e5fc1e314dc3c043
Philip Withnall <pwithnall@endlessos.org>
2023-07-17 11:41:22 +0100

    scripts: Fix looking up the kolibri-explore-plugin version
    The location of the version changed in release 6.27.0, as the build
    system changed to generate the version number from git.
    
    Since `_version.py` is a Python file, we can just import it rather than
    trying to build a rudimentary Python parser. Suggestion by Will Thompson
    and Dan Nicholson.
    
    Signed-off-by: Philip Withnall <pwithnall@endlessos.org>
    Fixes: https://github.com/endlessm/kolibri-explore-plugin/issues/705

--- scripts/version.py
+++ scripts/version.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import importlib
 import os
 import subprocess
 import sys
@@ -32,6 +33,23 @@ def explore_plugin_version_name():
 
 
 def explore_plugin_version():
+    # kolibri-explore-plugin v6.27.0 and newer store the version in a separate
+    # file, so try that first. Then fall back to parsing __init__.py, which is
+    # what older versions used.
+    try:
+        spec = importlib.util.spec_from_file_location(
+            "_version", f"./{EXPLOREPLUGIN_TARGET}/kolibri_explore_plugin/_version.py"
+        )
+        if not spec:
+            raise ImportError
+
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+
+        return getattr(module, "__version__")
+    except (ImportError, FileNotFoundError):
+        pass
+
     with open(
         f"./{EXPLOREPLUGIN_TARGET}/kolibri_explore_plugin/__init__.py", "r"
     ) as version_file:
